777f1356df6aeda9d161ceff2dfecca89ac272ea
Tommaso Castiglione Ferrari <tommaso@heurolabs.com>
2021-05-05 16:34:33 +0200

    Fixed partB so it uses the give tokenization

--- assignment_2/partB.py
+++ assignment_2/partB.py
@@ -1,48 +1,62 @@
 import spacy
+from spacy.tokens.doc import Doc
 
 nlp = spacy.load("en_core_web_sm")
 text = open("datasets/conllst.2017.trial.simple.conll", "r").read()
 text = text.split("\n")
-phrases = []
+phrases = {}
 phrase = []
+result = []
 index = 0
 
-# Extract information from the given file
-for i, tok in enumerate(text):
+# Iterates through the dataset and retrieves the text dividing it in different sentences based on the index numbers
+# The sentences are retrieved and aggregated to form a string, which is going to be used as key, and the tokenized
+#  elements are going to be saved in a list, saved as value for that key
+for tok in text:
     if not tok.replace(" \t", ""):
         continue
 
     token = tok.split("	")
-    print(token)
     if int(token[0]) != index + 1:
-        phrases.append(" ".join(phrase))
+        phr = " ".join(phrase).replace(" '", "'").replace(" .", ".").replace(" ,", ",").replace(" ;", ";").replace(" n't", "n't")
+        phrases[phr] = phrase
         phrase = []
 
     index = int(token[0])
     phrase.append(token[1])
 
-    if i == len(text) - 2:
-        phrases.append(" ".join(phrase))
-        phrase = []
+# The last phrase is not added to the dictionary in the for, so we're manually adding it here
+phr = " ".join(phrase).replace(" '", "'").replace(" .", ".").replace(" ,", ",").replace(" ;", ";").replace(" n't", "n't")
+phrases[phr] = phrase
 
+def tokenizer(txt: str) -> Doc:
+    """
+    This function overrides the spacy tokenizer, because we are using the already tokenized phrases retrieved from the dataset.
+    It creates and returns a Doc object containing the already tokenized sentence, which is going to go through the
+    remaining of the modules in the standard pipeline. Basically it skips spacy's tokenization
+    """
+    if txt in phrases:
+        return Doc(nlp.vocab, phrases[txt])
+    else:
+        raise ValueError('No tokenization available for input.')
 
+# Overrides spacy's tokenizer with our function
+nlp.tokenizer = tokenizer
+# Run spacy for each sentence and save the results in the "result" list (which is later going to be saved in a file)
+# NOTE: even if we are passing our normal string sentence, the tokenizer is overridden so we actually use the
+# tokenization retrieved from the dataset.
+for ph in phrases:
+    parser = nlp(ph)
 
-print(phrases[-1])
-
-result = []
-# Runs spacy for every sentence, and save the output in the "result" list
-for phrase in phrases:
-    doc = nlp(phrase)
-    for token in doc:
-        # print(token.i+1, token.text, token.lemma_, token.tag_, [elem.i for elem in doc if elem.text == str(token.head)][0]+1,
-        #       token.dep_)
+    for token in parser:
         result.append(f"{token.i+1}	"
                       f"{token.text}	"
                       f"{token.lemma_}	"
                       f"{token.tag_}	"
-                      f"{[elem.i for elem in doc if elem.text == str(token.head)][0]+1}	"
+                      f"{[elem.i for elem in parser if elem.text == str(token.head)][0]+1}	"
                       f"{token.dep_}")
     result.append(" ")
 
+# Join the results on multiple lines and save the final output
 open("datasets/cleaned_conllst_2017_trial_simple_conll", "w").write("\n".join(result))
 print("Done!")
