502f3e2be396b613c13317fa5103c2eaeae06e82
toetsie09 <s.brinkhuijsen@hotmail.com>
2021-05-07 22:38:37 +0200

    partC: Redid the stuff for Q8. The outputs are now written into csv files, which can be easily read into an online latex table generator (convenience)

--- assignment_2/partC.py
+++ assignment_2/partC.py
@@ -1,5 +1,6 @@
 import re
 import pprint
+import pandas as pd
 
 # --------------- Loading the files into lists ---------------
 
@@ -55,54 +56,47 @@ for sentence in sentences_output:
 print("\nDoes the parser find a root node for every sentence?\n", root_check)
 
 # Is the token assigned the same dependency head/label
-results = {}
-average_error_head = []
-average_error_label = []
-dep_dif = []
+df_compare = pd.DataFrame(columns=['TokenID', 'Token#', 'Word', 'POS', 'Out_head', 'Gold_head', 'Out_label', 'Gold_label'
+                                  , 'Out_deps', 'Gold_deps'])
+
+tokenID = 1
 for i, sentence in enumerate(sentences_output):
-    errors_head = 0
-    errors_label = 0
-
-    dependent_dict_gold = {}
-    dependent_dict_output = {}
-
     for j, word in enumerate(sentence):
-        # Check for same dependency head
-        if word[4] != sentences_gold[i][j][4]:
-            errors_head += 1
-        # Check for same dependency label
-        if word[5] != sentences_gold[i][j][5]:
-            errors_label += 1
 
-        # Check for same dependents
-        if sentences_gold[i][j][4] in dependent_dict_gold:
-            dependent_dict_gold[sentences_gold[i][j][4]].append(sentences_gold[i][j][0])
-        else:
-            dependent_dict_gold[sentences_gold[i][j][4]] = [sentences_gold[i][j][0]]
+        # Create a list with dependent children for the word in the output file
+        output_dep = [w[0] for w in sentence if w[4] == word[0]]
 
-        if word[4] in dependent_dict_output:
-            dependent_dict_output[word[4]].append(word[0])
-        else:
-            dependent_dict_output[word[4]] = [word[0]]
+        # Create a list with dependent children for the word in the golden file
+        golden_dep = [w[0] for w in sentences_gold[i] if w[4] == word[0]]
 
-    # Check if the tokens have the same dependents
-    if dependent_dict_gold != dependent_dict_output:
-        # print('The tokens in the output has the same dependents as in the golden file')
-        dep_dif.append(False)
-    else:
-        dep_dif.append(True)
+        # Add the token with information from both files to a pandas dataframe
+        entry = {
+            'TokenID': tokenID,
+            'Token#': word[0],
+            'Word': word[1],
+            'POS': word[3],
+            'Out_head': word[4],
+            'Gold_head': sentences_gold[i][j][4],
+            'Out_label': word[5],
+            'Gold_label': sentences_gold[i][j][5],
+            'Out_deps': output_dep,
+            'Gold_deps': golden_dep
+        }
+        df_compare = df_compare.append(entry, ignore_index=True)
+        tokenID += 1
 
-    average_error_head.append(round(errors_head/len(sentence), 2))
-    average_error_label.append(round(errors_label/len(sentence), 2))
+# Create a second dataframe that only contains tokens with at least 1 error.
+df_errors = df_compare[(df_compare['Out_head'] != df_compare['Gold_head']) | (df_compare['Out_label'] !=
+                        df_compare['Gold_label']) | (df_compare['Out_deps'] != df_compare['Gold_deps'])]
 
-print("\nnumber of mislabeled dependency head per sentence:\n", average_error_head)
-print("\nnumber of mislabeled dependency label per sentence:\n", average_error_label)
-print('\nAll tokens have the same dependents as in the gold file?\n', dep_dif)
-
+# insert the first 50 errors into a csv-file for a latex table generator
+df_errors.head(50).to_csv(r'./errors.csv', index=False)
 
 # --------------- Q9 Error POS ---------------
 
+# Create dictionary to count how often each POS tag occurs
 count_dict = {}
+# Create dictionary to count how often each POS tag is mislabeled
 error_dict = {}
 for i, sentence in enumerate(sentences_gold):
     for j, token in enumerate(sentence):
@@ -114,23 +108,26 @@ for i, sentence in enumerate(sentences_gold):
         if token != sentences_output[i][j]:
             error_dict[token[3]] += 1
 
+# Calculate the percentage of error
 error_pos = {}
 for value in list(count_dict):
     error_pos[value] = round(error_dict[value] / count_dict[value], 3)
 
+# Display the resulting error percentages per POS dictionary descending
 error_pos = dict(sorted(error_pos.items(), key=lambda item: item[1], reverse=True))
 print('\nSorted dictionary that displays the percentage of error per POS tag')
 pprint.pprint(error_pos, sort_dicts=False)
 
-# --------------- Q10 Error Label ---------------
+# insert dictionary into a csv-file for a latex table generator
+with open('pos_errors.csv', 'w') as f:
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in error_pos.items()]
 
-print()
+# --------------- Q10 Error Label ---------------
+# Create dictionary to count how often each dependency label occurs
 count_dict = {}
+# Create dictionary to count how often each dependency label is mislabeled
 error_dict = {}
 for i, sentence in enumerate(sentences_gold):
-    if i == 16:
-        continue
-
     for j, token in enumerate(sentence):
         if token[5] in count_dict:
             count_dict[token[5]] += 1
@@ -141,10 +138,16 @@ for i, sentence in enumerate(sentences_gold):
         if token != sentences_output[i][j]:
             error_dict[token[5]] += 1
 
-error_pos = {}
+# Calculate the percentage of error
+error_label = {}
 for value in list(count_dict):
-    error_pos[value] = round(error_dict[value] / count_dict[value], 3)
+    error_label[value] = round(error_dict[value] / count_dict[value], 3)
 
-error_pos = dict(sorted(error_pos.items(), key=lambda item: item[1], reverse=True))
+# Display the resulting error percentages per label dictionary descending
+error_label = dict(sorted(error_label.items(), key=lambda item: item[1], reverse=True))
 print('Sorted dictionary that displays the percentage of error per dependency label')
-pprint.pprint(error_pos, sort_dicts=False)
+pprint.pprint(error_label, sort_dicts=False)
+
+# insert dictionary into a csv-file for a latex table generator
+with open('label_errors.csv', 'w') as f:
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in error_label.items()]
